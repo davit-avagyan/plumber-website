8e2774b6e3b641ffdfcfc6e7cb8aafc74e7423f9
Davit Avagyan <davitavagyanweb@gmail.com>
2023-10-04 11:08:32 +0400

    added assets in config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   app: {
-    baseURL: '/plumber-website/'
+    baseURL: '/plumber-website/',
+    buildAssetsDir: 'assets',
   },
   devtools: { enabled: true },
   modules: [
