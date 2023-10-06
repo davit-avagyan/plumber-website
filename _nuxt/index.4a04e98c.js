84791ed038dcc22e930120700d6f3405ca14cb3e
davit-avagyan <62245803+davit-avagyan@users.noreply.github.com>
2023-10-06 07:20:06 +0000

    Deploying to gh-pages from @ davit-avagyan/plumber-website@dbce4a605790972eb29af0cd4faa5ef35a9e2f48 🚀

---
+++ _nuxt/index.4a04e98c.js
@@ -0,0 +1,1 @@
+import{c as e,o as t}from"./entry.ea71edad.js";const a={__name:"index",setup(c){return(o,r)=>(t(),e("h1",null,"Products"))}};export{a as default};
---
+++ _nuxt/logo-1.f2c764cf.js
@@ -0,0 +1,1 @@
+import"./entry.ea71edad.js";const r=""+new URL("logo-1.7ca59999.png",import.meta.url).href;export{r as _};
